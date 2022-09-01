d02dc8e4e65c93b7b3f74ceed29efecfdd3eb757
Vladimir <vladimirkuvanovv@gmail.com>
2022-09-01 22:58:58 +0300

    upd stylish

--- src/formatters/renderStylish.js
+++ src/formatters/renderStylish.js
@@ -1,58 +1,60 @@
 import _ from 'lodash';
 import {
-  ADDED_TYPE, REMOVED_TYPE, CHANGED_TYPE, UNCHANGED_TYPE, NESTED_TYPE,
+  ADDED_TYPE,
+  REMOVED_TYPE,
+  CHANGED_TYPE,
+  UNCHANGED_TYPE,
+  NESTED_TYPE,
 } from './states.js';
 
-const INDENT = '    ';
 const EOL = '\n';
 
-const stringify = (value, parentOffset, level = 0) => {
-  if (typeof value === 'boolean') {
-    return value ? 'true' : 'false';
+const stringify = (value, level, replacer) => {
+  if (!_.isObject(value)) {
+    return `${value}`;
   }
 
-  if (typeof value !== 'object') {
-    return value;
-  }
+  const indentForKey = replacer.repeat(level + 1);
+  const indentForBracket = replacer.repeat(level);
+  const lines = Object
+    .entries(value)
+    .map(([key, data]) => `${indentForKey}${key}: ${stringify(data, level + 1, replacer)}`);
 
-  if (value === null) {
-    return value;
-  }
-
-  parentOffset = level ? parentOffset : INDENT;
-  const offset = _.repeat(INDENT, level + 1);
-
-  const keys = Object.keys(value);
-  const nestedItem = keys.map((key) => `${parentOffset}${offset}${key}: ${stringify(value[key], parentOffset, level + 1)}`);
-
-  return `{${EOL}${nestedItem.join(EOL)}${EOL}${offset}}`;
+  return ['{', ...lines, `${indentForBracket}}`].join(EOL);
 };
 
-const buildStylish = (tree, level = 0) => {
-  const offset = _.repeat(INDENT, level);
+const sign = {
+  ADDED_TYPE: '+',
+  REMOVED_TYPE: '-',
+  UNCHANGED_TYPE: ' ',
+};
 
-  const stylishNodes = tree.map((node) => {
+const renderStylish = (diff, replacer = '    ') => {
+  const iter = (tree, level) => tree.map((node) => {
+    const indent = replacer.repeat(level);
+    const indentForSign = indent.slice(2);
+
+    const makeLine = (value, mark) => `${indentForSign}${mark} ${node.key}: ${stringify(value, level, replacer)}`;
+
     switch (node.type) {
       case NESTED_TYPE:
-        // eslint-disable-next-line no-case-declarations
-        const newChildren = buildStylish(node.children, level + 1);
-        return `${INDENT}${offset}${node.key}: ${newChildren}`;
-      case UNCHANGED_TYPE:
-        return `${offset}${INDENT}${node.key}: ${stringify(node.dataAfter, offset, level)}`;
-      case CHANGED_TYPE:
-        return `${offset}  - ${node.key}: ${stringify(node.dataBefore, offset, level)}${EOL}${offset}  + ${node.key}: ${stringify(node.dataAfter, offset, level)}`;
-      case REMOVED_TYPE:
-        return `${offset}  - ${node.key}: ${stringify(node.dataBefore, offset, level)}`;
+        return `${indent}${node.key}: ${['{', ...iter(node.children, level + 1), `${indent}}`].join(EOL)}`;
       case ADDED_TYPE:
-        return `${offset}  + ${node.key}: ${stringify(node.dataAfter, offset, level)}`;
+        return makeLine(node.dataAfter, sign.ADDED_TYPE);
+      case REMOVED_TYPE:
+        return makeLine(node.dataBefore, sign.REMOVED_TYPE);
+      case UNCHANGED_TYPE:
+        return makeLine(node.dataBefore, sign.UNCHANGED_TYPE);
+      case CHANGED_TYPE:
+        return [`${makeLine(node.dataBefore, sign.REMOVED_TYPE)}`,
+          `${makeLine(node.dataAfter, sign.ADDED_TYPE)}`].join('\n');
       default:
-        return `unknown ${node.type}`;
+        throw new Error(`Type: ${node.type} is undefined`);
     }
   });
 
-  return `{${EOL}${stylishNodes.join(EOL)}${EOL}${offset}}`;
+  const stylishDiff = iter(diff, 1);
+  return ['{', ...stylishDiff, '}'].join('\n');
 };
-
-const renderStylish = (tree) => buildStylish(tree);
 
 export default renderStylish;
